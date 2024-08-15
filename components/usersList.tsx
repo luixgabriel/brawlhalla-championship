dc1c387072f88b913dbf6cbf33d8369483368a98
Luis Gabriel <luisgasbriel@gmail.com>
2024-08-15 20:12:52 -0300

    feat: adicionando a lógica das vitórias e finalizando a tela

--- components/usersList.tsx
+++ components/usersList.tsx
@@ -1,17 +1,37 @@
 import { Image, ScrollView, Text, View } from "react-native";
 import { Users } from "../types/usersType";
 
-export default function UsersList({ avatar, id, name, victorys }: Users) {
+export default function UsersList({ avatar, name, victorys }: Users) {
+  function getVictoryBarWidth() {
+    const maxVictorys = 10;
+    const maxWidth = 100;
+
+    if (victorys < 0) victorys = 0;
+    if (victorys > maxVictorys) victorys = maxVictorys;
+
+    return (victorys / maxVictorys) * maxWidth;
+  }
+
+  const displayName = victorys <= 3 ? `${name.slice(0, -3)}...` : name;
+
   return (
-    <View>
-      <View className="flex my-3 flex-row items-center">
+    <View className="pr-32 pl-10 w-full">
+      <View className="flex my-3 flex-row items-center w-full">
         <Image
           source={{ uri: avatar }}
           resizeMode="contain"
           className="w-[60px] h-[60px] rounded-full"
         />
-        <View className="bg-primary h-[30px] w-[100px] flex justify-center mx-2">
-          <Text className="text-md px-2">{name}</Text>
+        <View
+          style={{ width: `${getVictoryBarWidth()}%` }}
+          className="bg-primary h-[30px] flex justify-center mx-2"
+        >
+          <Text
+            style={{ fontFamily: "BlackOpsOne_400Regular" }}
+            className="text-lg px-2 text-white"
+          >
+            {displayName}
+          </Text>
         </View>
       </View>
     </View>
